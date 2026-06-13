2f67c0d72baa6ad97c89f625908d27e055cb0904
Alessia La Mura <alessialamura33@gmail.com>
2026-06-13 18:14:35 +0200

    aggiunto react hook form x login e register

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,7 +1,21 @@
+import { useForm } from "react-hook-form";
+
 export default function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const showdata = (data) => {
+    console.log(data);
+  };
+
   return (
     <>
-      <form className="flex justify-center items-center h-screen">
+      <form
+        onSubmit={handleSubmit(showdata)}
+        className="flex justify-center items-center h-screen"
+      >
         <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
           <legend className="fieldset-legend">Login</legend>
 
@@ -13,7 +27,13 @@ export default function Login() {
             type="email"
             className="input"
             placeholder="Email"
+            {...register("email", { required: true, maxLength: 50 })}
           />
+          {errors.email && (
+            <span className="text-error">
+              Questo campo è obbligatorio e può avere al massimo 50 caratteri
+            </span>
+          )}
 
           <label className="label" htmlFor="login-password">
             Password
@@ -23,9 +43,15 @@ export default function Login() {
             type="password"
             className="input"
             placeholder="Password"
+            {...register("password", { required: true, maxLength: 50 })}
           />
+          {errors.password && (
+            <span className="text-error">
+              Questo campo è obbligatorio e può avere al massimo 50 caratteri
+            </span>
+          )}
 
-          <button type="button" className="btn btn-neutral mt-4">
+          <button type="submit" className="btn btn-neutral mt-4">
             Login
           </button>
         </fieldset>
--- src/views/Register.jsx
+++ src/views/Register.jsx
@@ -1,50 +1,65 @@
 import { useContext, useState } from "react";
 import { UserContext } from "../context/UserContext";
 import { useNavigate } from "react-router";
+import { useForm } from "react-hook-form";
 export default function Register() {
-  const { register } = useContext(UserContext);
-
-  const [userName, setUserName] = useState("");
-  const [userEmail, setUserEmail] = useState("");
-
-  const navigate = useNavigate();
-
-  const handleChange = (e) => {
-    if (e.target.name === "name") {
-      setUserName(e.target.value);
-    } else if (e.target.name === "email") {
-      setUserEmail(e.target.value);
-    }
+  // const { register } = useContext(UserContext);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const showdata = (data) => {
+    console.log(data);
   };
 
-  const handlesubmit = (e) => {
-    e.preventDefault();
-    if (userName && userEmail) {
-      register({ name: userName, email: userEmail });
-    }
-    navigate("/");
-  };
+  // const [userName, setUserName] = useState("");
+  // const [userEmail, setUserEmail] = useState("");
+
+  // const navigate = useNavigate();
+
+  // const handleChange = (e) => {
+  //   if (e.target.name === "name") {
+  //     setUserName(e.target.value);
+  //   } else if (e.target.name === "email") {
+  //     setUserEmail(e.target.value);
+  //   }
+  // };
+
+  // const handlesubmit = (e) => {
+  //   e.preventDefault();
+  //   if (userName && userEmail) {
+  //     register({ name: userName, email: userEmail });
+  //   }
+  //   navigate("/");
+  // };
 
   return (
     <>
       <form
-        onSubmit={handlesubmit}
+        onSubmit={handleSubmit(showdata)}
         className="flex justify-center items-center h-screen"
       >
         <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
           <legend className="fieldset-legend">Register</legend>
 
           <label className="label" htmlFor="register-name">
-            Name
+            Username
           </label>
           <input
             id="register-name"
             type="text"
             className="input"
-            placeholder="Name"
-            onChange={handleChange}
+            placeholder="Username"
+            // onChange={handleChange}
             name="name"
+            {...register("username", { required: true, maxLength: 50 })}
           />
+          {errors.username && (
+            <span className="text-error">
+              Questo campo è obbligatorio e può avere al massimo 50 caratteri
+            </span>
+          )}
 
           <label className="label" htmlFor="register-email">
             Email
@@ -54,9 +69,31 @@ export default function Register() {
             type="email"
             className="input"
             placeholder="Email"
-            onChange={handleChange}
+            // onChange={handleChange}
             name="email"
+            {...register("email", { required: true, maxLength: 50 })}
           />
+          {errors.email && (
+            <span className="text-error">
+              Questo campo è obbligatorio e può avere al massimo 50 caratteri
+            </span>
+          )}
+
+          <label className="label" htmlFor="register-password">
+            Password
+          </label>
+          <input
+            id="register-password"
+            type="password"
+            className="input"
+            placeholder="Password"
+            {...register("password", { required: true, maxLength: 50 })}
+          />
+          {errors.password && (
+            <span className="text-error">
+              Questo campo è obbligatorio e può avere al massimo 50 caratteri
+            </span>
+          )}
 
           <button type="submit" className="btn btn-neutral mt-4">
             Register
